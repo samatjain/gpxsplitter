89fc259121f08681330ef3573bc0c4bf2b41b325
Samat K Jain <samat@samat.org>
2011-07-20 15:46:39 -0600

    Detect & process both GPX 1.0 and 1.1 track files

--- gpxsplitter.py
+++ gpxsplitter.py
@@ -16,8 +16,6 @@ import pprint
 from lxml import etree
 from mx.DateTime.ISO import ParseDateTimeUTC
 
-GPX_NS = 'http://www.topografix.com/GPX/1/1',
-
 TrackTuple = collections.namedtuple(
     'TrackTuple',
     ['track_etree',
@@ -50,21 +48,36 @@ def determineBoundingBox(points):
 
 
 def go(document):
+    # Detect whether GPX 1.0 or 1.1 is in use
+    gpx_ns = None
+    gpx_namespaces = ['http://www.topografix.com/GPX/1/0',
+                      'http://www.topografix.com/GPX/1/1']
+
+    t = document.getroot().tag
+    for ns in gpx_namespaces:
+        if ns in t:
+            gpx_ns = ns
+
+    # TODO: Turn this into an exception
+    if gpx_ns is None:
+        print >> sys.stderr, 'Unable to determine GPX version (neither 1.0 or 1.1)' % prog_name
+        sys.exit(1)
+
     track_objects = []
-    track_elements = document.findall('{%s}trk' % GPX_NS)
+    track_elements = document.findall('{%s}trk' % gpx_ns)
     for track_element in track_elements:
         document.getroot().remove(track_element)
 
         # Remove track numbering
-        number = track_element.find('{%s}number' % GPX_NS)
+        number = track_element.find('{%s}number' % gpx_ns)
         if number is not None:
             track_element.remove(number)
 
-        points = track_element.findall('.//{%s}trkpt' % GPX_NS)
+        points = track_element.findall('.//{%s}trkpt' % gpx_ns)
         bbox = determineBoundingBox(points)
 
-        start_date = ParseDateTimeUTC(points[0].find('{%s}time' % GPX_NS).text)
-        end_date = ParseDateTimeUTC(points[-1].find('{%s}time' % GPX_NS).text)
+        start_date = ParseDateTimeUTC(points[0].find('{%s}time' % gpx_ns).text)
+        end_date = ParseDateTimeUTC(points[-1].find('{%s}time' % gpx_ns).text)
 
         track_obj = TrackTuple(
             track_etree=track_element, num_points=len(points),
@@ -73,9 +86,9 @@ def go(document):
             waypoints=[])
         track_objects.append(track_obj)
 
-    waypoints = document.findall('//{%s}wpt' % GPX_NS)
+    waypoints = document.findall('//{%s}wpt' % gpx_ns)
     for waypoint in waypoints:
-        time = ParseDateTimeUTC(waypoint.find('{%s}time' % GPX_NS).text)
+        time = ParseDateTimeUTC(waypoint.find('{%s}time' % gpx_ns).text)
         document.getroot().remove(waypoint)
 
         # If waypoint was recording in a track's time interval, store
@@ -83,7 +96,7 @@ def go(document):
             if track.start_date <= time and time <= track.end_date:
                 track.waypoints.append(waypoint)
                 continue
-            #print 'Waypoint %s could not be placed' % waypoint.find('{%s}name' % GPX_NS).text
+            #print 'Waypoint %s could not be placed' % waypoint.find('{%s}name' % gpx_ns).text
 
     document_template = document
 
@@ -94,21 +107,21 @@ def go(document):
         filename = t.end_date.strftime('%Y-%m-%dT%H:%M:%SZ') + '.gpx'
 
         # Set metadata time to time of latest trackpoint
-        metadata_tag = document.find('{%s}metadata' % GPX_NS)
+        metadata_tag = document.find('{%s}metadata' % gpx_ns)
         if metadata_tag is None:
-            metadata_tag = etree.Element('{%s}metadata')
+            metadata_tag = etree.Element('{%s}metadata' % gpx_ns)
             document.getroot().append(metadata_tag)
 
-        metadata_time_tag = metadata_tag.find('{%s}time' % GPX_NS)
+        metadata_time_tag = metadata_tag.find('{%s}time' % gpx_ns)
         if metadata_time_tag is None:
-            metadata_time_tag = etree.Element('{%s}time')
+            metadata_time_tag = etree.Element('{%s}time' % gpx_ns)
             metadata_tag.append(metadata_time_tag)
         metadata_time_tag.text = t.end_date.strftime('%Y-%m-%dT%H:%M:%SZ')
 
         # Set bounds based on trackpoints going into this file
-        bounds_tag = metadata_tag.find('.//{%s}bounds' % GPX_NS)
+        bounds_tag = metadata_tag.find('.//{%s}bounds' % gpx_ns)
         if bounds_tag is None:
-            bounds_tag = etree.Element('{%s}bounds')
+            bounds_tag = etree.Element('{%s}bounds' % gpx_ns)
             metadata_tag.append(bounds_tag)
         for attrib_name in ('minlat', 'minlon', 'maxlat', 'maxlon'):
             bounds_tag.attrib[attrib_name] = t.bounding_box[attrib_name]
